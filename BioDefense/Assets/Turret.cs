1039f7f1ab620824ba034c6a02cce6b863b5e9ee
João Vieira <1190112@isep.ipp.pt>
2023-11-29 00:26:49 +0000

    added bullet

--- BioDefense/Assets/Turret.cs
+++ BioDefense/Assets/Turret.cs
@@ -8,12 +8,16 @@ public class Turret : MonoBehaviour
     [Header("References")]
     [SerializeField] private Transform turretRotationPoint;
     [SerializeField] private LayerMask enemyMask;
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firingPoint;
 
     [Header("Attribute")]
     [SerializeField] private float targetingRange = 5f;
     [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private float bps = 1f; //bullets per second
 
     private Transform target;
+    private float timeUntilFire;
 
     private void OnDrawGizmosSelected()
     {
@@ -64,5 +68,22 @@ public class Turret : MonoBehaviour
         {
             target = null;
         }
+        else
+        {
+            timeUntilFire += Time.deltaTime;
+
+            if(timeUntilFire >= 1f / bps)
+            {
+                Shoot();
+                timeUntilFire = 0f;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bulletObj = Instantiate(bulletPrefab, firingPoint.position, Quaternion.identity);
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+        bulletScript.SetTarget(target);
     }
 }
