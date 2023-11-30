5a2cfbd46ab804dfc7f24cf6c7e0d06c853a4645
João Vieira <1190112@isep.ipp.pt>
2023-11-30 20:40:02 +0000

    added new enemy iceTower

--- BioDefense/Assets/_Main/Scripts/EnemyMovement.cs
+++ BioDefense/Assets/_Main/Scripts/EnemyMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class EnemyMovement : MonoBehaviour
 {
     [Header("References")]
@@ -13,8 +14,11 @@ public class EnemyMovement : MonoBehaviour
     private Transform target;
     private int pathIndex = 0;
 
+    private float baseSpeed;
+
     private void Start()
     {
+        baseSpeed = moveSpeed;
         target = LevelManager.main.path[pathIndex];
     }
 
@@ -42,4 +46,15 @@ public class EnemyMovement : MonoBehaviour
         Vector2 direction = (target.position - transform.position).normalized;
         rb.velocity = direction * moveSpeed;
     }
+
+    public void UpdateSpeed(float newSpeed)
+    {
+        moveSpeed = newSpeed;
+    }
+
+    public void ResetSpeed()
+    {
+        moveSpeed = baseSpeed;
+    }
 }
+
---
+++ BioDefense/Assets/_Main/Scripts/IceTower.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class IceTower : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attribute")]
+    [SerializeField] private float targetingRange = 5f;
+    [SerializeField] private float aps = 5f;
+    [SerializeField] private float freezeTime = 1f;
+
+
+    private float timeUntilFire;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilFire += Time.deltaTime;
+
+        if (timeUntilFire >= 1f / aps)
+        {
+            FreezeEnemies();
+            timeUntilFire = 0f;
+        }
+    }
+
+    private void FreezeEnemies()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+
+        if(hits.Length > 0)
+        {
+            for(int i = 0; i < hits.Length; i++)
+            {
+                RaycastHit2D hit = hits[i];
+
+                EnemyMovement em = hit.transform.GetComponent<EnemyMovement>();
+                em.UpdateSpeed(0.5f);
+
+                StartCoroutine(ResetEnemySpeed(em));
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position, transform.forward, targetingRange);
+
+    }
+
+    private IEnumerator ResetEnemySpeed(EnemyMovement em)
+    {
+        yield return new WaitForSeconds(freezeTime);
+
+        em.ResetSpeed();
+    }
+
+}
